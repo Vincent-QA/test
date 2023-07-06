231573ed4f8e8f6453b612037dfc38dc8828524d
INNOTECH-VN\hoang <hoang@innotech-vn.com>
2023-07-06 10:00:57 +0700

    ghghhg

--- features/pageobjects/secure.page.ts
+++ features/pageobjects/secure.page.ts
@@ -2,13 +2,9 @@ import { ChainablePromiseElement } from 'webdriverio';
 
 import Page from './page.js';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
+
 class SecurePage extends Page {
-    /**
-     * define selectors using getter methods
-     */
+
     public get flashAlert () {
         return $('#flash');
     }
